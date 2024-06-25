00bafa87235eb39e3cb1dfa9b0e02ac1e7cccf31
Minh Pham <minh9812@gmail.com>
2024-06-24 23:10:11 -0400

    connectData

--- config.php
+++ config.php
@@ -7,11 +7,16 @@
 
     //Setup host
     define('_WEB_HOST_ROOT', 'http://'.$_SERVER['HTTP_HOST']
-    . '/user_manager'); // home address
+    . '/user_manager'); // home address: http://localhost/user_manager
 
     define('_WEB_HOST_TEMPLATES', _WEB_HOST_ROOT . '/templates');
 
     //Setup path
-    define('_WEB_PATH_ROOT', __DIR__);
+    define('_WEB_PATH_ROOT', __DIR__); ///Applications/XAMPP/xamppfiles/htdocs/user_manager
     define('_WEB_PATH_TEMPLATES', _WEB_PATH_ROOT . '/templates');
 
+    //Setup connect to Database
+    const _HOST = 'localhost';
+    const _USER = 'root';
+    const _PASSWORD = '';
+    const _DB = 'userManager';
--- includes/connect.php
+++ includes/connect.php
@@ -1,2 +1,10 @@
 <?php
     if(!defined('_INCODE')) die('Access Denied...');
+
+    require 'config.php';
+    try{
+        $conn = new PDO("mysql:host="._HOST.";dbname="._DB, _USER, _PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+        echo "Error: " . $e->getMessage();
+    }
