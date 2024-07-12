c616e9958f0f56853ad7a9a5c33b0d69ec35fdf4
Minh Pham <minh9812@gmail.com>
2024-07-11 23:18:12 -0400

    logout

--- modules/auth/logout.php
+++ modules/auth/logout.php
@@ -1,2 +1,8 @@
 <?php
     if(!defined('_INCODE')) die('Access Denied...');
+    if(isLogin()){
+        $token = getSession('loginToken');
+        delete('loginToken', "token=$token");
+        removeSession('loginToken');
+        redirect('?module=auth&action=login');
+    }
