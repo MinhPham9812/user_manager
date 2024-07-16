9daf744e4f0405e77e97ce1855edb8169d91e451
Minh Pham <minh9812@gmail.com>
2024-07-15 20:42:51 -0400

    pagination

--- modules/users/lists.php
+++ modules/users/lists.php
@@ -5,9 +5,39 @@
         'pageTitle' => 'User Manager'
     ];
     layout('header', $data);
+
+    //Handle pagination
+    // Count user
+    $allUserNum = getRows("SELECT * FROM users");
     
+    //1.Identify records on a page
+    $perPage = 3; //Each page has 3 records
+
+    //2.Calculate the number of pages
+    $maxPage = ceil($allUserNum/$perPage);
+
+    //3.Handle the page number based on the get method
+    $page = getBody()['page'];
+    if(!empty($page)){
+        //check if page wrong
+        if($page<1 || $page>$maxPage){
+            $page = 1;
+        }
+    }else{
+        $page = 1;
+    }
+
+    //4. Calculate offset in limit based on $page
+    /*
+        $page = 1 => offset = 0
+        $page = 2 => offset = 3
+        $page = 3 => offset = 6
+        => offset = ($page-1)*3
+    */
+    $offset = ($page-1)*3;
+
     //Get data from database
-    $listUser = getRaw("SELECT * FROM users ORDER BY updateAt");
+    $listUser = getRaw("SELECT * FROM users ORDER BY createAt LIMIT $offset, $perPage");
     
 ?>
     <div class="container">
@@ -35,7 +65,7 @@
                             $count++;
                 ?>
                 <tr>
-                    <td><?php echo $count; ?></td>
+                    <td><?php echo $item['id']; ?></td>
                     <td><?php echo $item['middlename']?$item['firstname'] .' '. $item['middlename'] .' '. $item['lastname'] :$item['firstname'] .' '. $item['lastname'];  ?></td>
                     <td><?php echo $item['email']; ?></td>
                     <td><?php echo $item['status']==1?'<button type="button" class="btn btn-success btn-sm">Activated</button>'
@@ -52,7 +82,39 @@
                 <?php endif; ?>
             </tbody>
         </table>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-end">
+                <?php
+                    $prevPage = $page - 1;
+                    if($page > 1){
+                       echo  '<li class="page-item"><a class="page-link" href=" '. _WEB_HOST_ROOT.'?module=users&page='.$prevPage .'">Previous</a></li>';
+                    }
+                    
+                ?>
+                
+                <?php 
+                $begin = $page - 2;
+                if($begin < 1){
+                    $begin = 1;
+                }
+                $end = $page + 2;
+                if($end > $maxPage){
+                    $end = $maxPage;
+                }
+                for($i=$begin; $i<=$end; $i++){ ?>
+                <li class="page-item <?php echo ($i==$page)?'active':false; ?>"><a class="page-link" href="<?php echo _WEB_HOST_ROOT.'?module=users&page='.$i; ?>"><?php echo $i; ?></a></li>
+                <?php }; ?>    
+                <?php
+                    $nextPage = $page + 1;
+                    if($page < $maxPage){
+                        echo '<li class="page-item"><a class="page-link" href="'. _WEB_HOST_ROOT.'?module=users&page='.$nextPage .'">Next</a></li>';
+                    }
+                ?>
+                
+            </ul>
+        </nav>
     </div>
+    <hr>
 <?php
     layout('footer');
 ?>
